3fdd6d455f606c07e60382d18d14041e22349000
pohrebacek <petrjan872@gmail.com>
2026-05-29 12:41:53 +0200

    luckers gambit added

---
+++ src/LuckersGambit/Main.java
@@ -0,0 +1,33 @@
+import core.GameHub;
+import model.Player;
+import ui.InputHandler;
+
+/**
+ * Hlavni spousteci trida pro program
+ */
+public class Main {
+
+    static void initialize(){
+        String prompt = "Zacatek aplikace, prejete si zacit novou hru, nebo navazat na jiz existujici?";
+        String newGame = "Zacatek nove hry";
+        String loadGame = "Nacteni hry (TO-DO)";
+        String end = "Konec";
+        System.out.println("LuckersGamibit App:");
+        int choice = InputHandler.readChoices(prompt, newGame, loadGame, end);
+        if (choice == 0){
+            GameHub hub = new GameHub(Player.createPlayer());
+            hub.hubLoop();
+        } else if (choice == 1) {
+            System.out.println("Not yet implemented :(");
+            // TODO: 30.04.2025 LOAD mechinismus
+        } else {
+            System.out.println("Ukoncuji...");
+        }
+    }
+
+    public static void main(String[] args) {
+        // TODO: 30.04.2025 Scanner input text -> cislo a vice versa 
+        
+        initialize();
+    }
+}
---
+++ src/LuckersGambit/core/Roulette.java
@@ -0,0 +1,38 @@
+package core;
+
+import assets.RandomGen;
+import ui.InputHandler;
+
+public class Roulette extends Game {
+
+    private final double betMultiplyer = 1.6;
+    public Roulette(String gameName, int minBet) {
+        super(gameName, minBet);
+    }
+    
+    @Override
+    public void startGame() {
+        System.out.println("Priprava na roulette hru...");
+        System.out.println("Vsazeno " + getInputBet());
+        player.decreaseBalance(getInputBet());
+        int winnings = getInputBet();
+        int deathNumber = RandomGen.getRandomInt(1, 6);
+        System.out.println("CHEAT: " + deathNumber);
+        int counter = 0;
+        int choice = InputHandler.readChoices("Zacatek rulety, prejete si risknout?", "Ano", "Ne");
+        while (choice != 1){
+            System.out.println("Ruleta...");
+            counter++;
+            if (counter != deathNumber){
+                winnings *= betMultiplyer;
+                choice = InputHandler.readChoices("Prezil jste. Prejete si risknout dalsi pokus?", "Ano", "Ne");
+            } else {
+                System.out.println("Ruletu jste prohral");
+                winnings = 0;
+                break;
+            }
+        }
+        System.out.println("Vysledek hry: " + winnings);
+        player.increaseBalance(winnings);
+    }
+}
